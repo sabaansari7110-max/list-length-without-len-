8125c6e2f9c907f2afc33b8b8e84534773015a97
Saba Ansari <sabaansari7110@gmail.com>
2026-01-18 15:49:49 +0530

    Add files via upload

---
+++ list lenght without len().py
@@ -0,0 +1,17 @@
+#(1st method)
+numbers= [10,20,30]
+
+for i in numbers:
+    print(i)
+
+#(2nd method)
+count= 0
+for i in numbers:
+    count= count + 1
+print(count)
+
+#(3rd method)
+count= 0
+for i in numbers:
+    count= count + 1
+    print(count)
